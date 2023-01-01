87723aa057a7521a68129abfb4a8c16c3ea5d587
Amin <theaminux@gmail.com>
2023-01-01 20:58:17 +0300

    Integrated PyPDF2

--- converter/converter.py
+++ converter/converter.py
@@ -4,7 +4,7 @@ import os
 import re
 import time
 import pdftotext
-from pprint import pprint
+from PyPDF2 import PdfReader
 
 
 def convert_pdf_to_text():
@@ -12,15 +12,17 @@ def convert_pdf_to_text():
     for file in os.listdir("./corpus/pdf"):
         if file.endswith(".pdf"):
             with open(f"./corpus/pdf/{file}", "rb") as pdf_file:
-                pdf = pdftotext.PDF(pdf_file)
+                reader = PdfReader(pdf_file)
                 with open(f"./corpus/text/{file[:-4]}.txt", "w") as text_file:
-                    for page in pdf:
-                        text_file.write(page)
+                    for page in reader.pages:
+                        text = page.extract_text()
+                        text_file.write(text)
+                        # text_file.write(page)
                 print(f"{file} converted to text file successfully.")
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    # convert_pdf_to_text()
+    convert_pdf_to_text()
 
     print(f"--- {time.time() - start_time} seconds ---")
