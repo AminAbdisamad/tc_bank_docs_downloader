73999f7af2de1be70d31a04c49b5a1c6933cc9cf
Amin <theaminux@gmail.com>
2023-01-01 20:37:26 +0300

    add extractor

--- converter/converter.py
+++ converter/converter.py
@@ -4,11 +4,7 @@ import os
 import re
 import time
 import pdftotext
-
-# import PyPDF2
 from pprint import pprint
-
-# from downloader.constants import YEARS
 
 
 def convert_pdf_to_text():
@@ -17,23 +13,14 @@ def convert_pdf_to_text():
         if file.endswith(".pdf"):
             with open(f"./corpus/pdf/{file}", "rb") as pdf_file:
                 pdf = pdftotext.PDF(pdf_file)
-                # number_of_pages = len(pdf)
-                year = file[:4]
-                # if year.isdigit() and int(year) in YEARS:
                 with open(f"./corpus/text/{file[:-4]}.txt", "w") as text_file:
                     for page in pdf:
                         text_file.write(page)
-                        # for page_number in range(number_of_pages):
-                        #     page = read_pdf.getPage(page_number)
-                        #     page_content = page.extractText()
-                        # text_file.write(page_content)
-                # else:
-                #     print(f"Invalid year: {year}")
-
                 print(f"{file} converted to text file successfully.")
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    convert_pdf_to_text()
+    # convert_pdf_to_text()
+
     print(f"--- {time.time() - start_time} seconds ---")
---
+++ converter/extractor.py
@@ -0,0 +1,20 @@
+import os, re
+from pprint import pprint
+
+# Get date example (03 January 2002) from the text files in the corpus folder
+# and save it in a dictionary with context as value and date as key
+def get_date_from_text_files():
+    """Get date example (03 January 2002) from the text files in the corpus folder and save it in a dictionary with context as value and date as key."""
+    dates = {}
+    for file in os.listdir("./corpus/text"):
+        if file.endswith(".txt"):
+            with open(f"./corpus/text/{file}", "r") as text_file:
+                text = text_file.read()
+                date = re.search(r"\d{2} \w+ \d{4}", text)
+                if date:
+                    dates[date.group()] = text
+    return dates
+
+
+dates = get_date_from_text_files()
+pprint(dates)
